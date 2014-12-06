3e2991a6831fe4ea39a1e8c261377d64b00dd5fa
Matthew Andrews <matt@mattandre.ws>
2014-12-06 15:36:06 +0000

    Quick hack together of a image resizing helper with handlebars

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var expressHandlebars = require('express-handlebars');
+app.engine('.handlebars', expressHandlebars({
+	extname: '.handlebars',
+	helpers: {
+		'resize': function(width, options) {
+			return '//image.webservices.ft.com/v1/images/raw/' + encodeURIComponent(options.fn(this)) + '?width=' + width + '&source=docs&fit=scale-down';
+		}
+	}
+}));
+app.set('view engine', '.handlebars');
+
+app.get('/', function(req, res, next) {
+	res.render('main', {
+		title: "FT",
+		image: "https://avatars0.githubusercontent.com/u/3502508?v=3"
+	});
+});
+
+app.listen(process.env.PORT);
